a815316c900b5e2a6aefbd9991f81417dda90cfa
Mohamed Ramadan <mr01028760097@gmail.com>
2022-08-04 19:41:34 +0200

    configure store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { cryptoApi } from './../services/cryptoApi';
-import { cryptoNewsApi } from '../services/cryptoNewsApi';
+import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "./../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
+import { exchangesApi } from "./../services/exchangesApi";
 export const store = configureStore({
-    reducer: {
-        [cryptoApi.reducerPath]: cryptoApi.reducer,
-        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(cryptoApi.middleware),
-
-})
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+    [exchangesApi.reducerPath]: exchangesApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
+});
